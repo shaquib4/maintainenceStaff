7faf9e72a4c22f76a774c825357f26ab82562135
Ekagra Agrawal <ekagraagrawal49@gmail.com>
2021-11-14 19:42:07 +0530

    Pending Requests

--- app/src/main/java/com/example/bietmaintainencestaff/ActivityPendingRequests.kt
+++ app/src/main/java/com/example/bietmaintainencestaff/ActivityPendingRequests.kt
@@ -2,10 +2,38 @@ package com.example.bietmaintainencestaff
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bietmaintainencestaff.Modals.ModalPendingRequest
+import com.google.firebase.database.*
 
 class ActivityPendingRequests : AppCompatActivity() {
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var listPendingRequests: List<ModalPendingRequest>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pending_requests)
+        listPendingRequests = ArrayList<ModalPendingRequest>()
+        databaseReference = FirebaseDatabase.getInstance().reference.child("Staff").child("JE").child("Requests")
+        databaseReference.child("PendingRequests").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                (listPendingRequests as ArrayList<ModalPendingRequest>).clear()
+                for (i in snapshot.children) {
+                    val obj = ModalPendingRequest(
+                        i.child("requestId").value.toString(),
+                        i.child("roomNo").value.toString(),
+                        i.child("requestedBy").value.toString(),
+                        i.child("problem").value.toString(),
+                        i.child("hostelName").value.toString(),
+                        i.child("mobNo").value.toString(),
+                        i.child("status").value.toString(),
+                    )
+                    (listPendingRequests as ArrayList<ModalPendingRequest>).add(obj)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 }
---
+++ app/src/main/java/com/example/bietmaintainencestaff/Modals/ModalPendingRequest.kt
@@ -0,0 +1,14 @@
+package com.example.bietmaintainencestaff.Modals
+
+class ModalPendingRequest(
+    val requestId:String,
+    val roomNo:String,
+    val requestedBy:String,
+    val problem:String,
+    val hostelName:String,
+    val mobNo:String,
+    val status:String
+
+) {
+    constructor():this("","","","","","","")
+}
